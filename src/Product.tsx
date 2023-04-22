bf208882b7b5cf6552f73143c7e24d1d0cecd221
Harold <harold@pop-os.localdomain>
2023-04-22 18:21:30 -0400

    Product card

--- src/Product.tsx
+++ src/Product.tsx
@@ -5,7 +5,7 @@ export default function Product() {
 
 return (
     <div className="flex flex-col group cursor-pointer bg-slate-800 rounded-xl p-4" >
-            <a href="#"className="h-[20vh] border h-[60vh] border-teal-200  group-hover:outline-double outline-orange-500 outline-2 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 group-hover:scale-[1.01] transition ">
+            <a href="#"className="border h-[20vh] border-teal-200  group-hover:outline-double outline-orange-500 outline-2 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 group-hover:scale-[1.01] transition ">
                 <img className="object-cover h-full rounded-t-lg" src="https://imgv3.fotor.com/images/blog-cover-image/Image-Upscaler-2.jpg" alt="" />
             </a>
 
--- src/Products.tsx
+++ src/Products.tsx
@@ -8,6 +8,7 @@ export default function Products() {
       <h2 className="text-2xl font-bold text-white">Other Projects</h2>
 
       <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 lg:gap-y-6 ">
+        <Product />
       </div>
 
       
